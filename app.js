82345ce52cf627e7873b542f1bf1d124c4903fd3
JeromeArnoult <jerome.arnoult.pro@proton.me>
2024-03-16 18:40:48 +0100

    Css Accueil et media Queries

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const ligne = document.querySelector('.cont-ligne');
 
 btnMenu.addEventListener('click',() => {
     ligne.classList.toggle('active')
+    nav.classList.toggle('menu-visible')
 });
 
+if(window.matchMedia('(max-width: 1300px')) {
+    allItemNav.forEach(item=>{
+        item.addEventListener('click',()=>{
+            nav.classList.toggle('menu-visible')
+            ligne.classList.toggle('active');
+        })
+    })
+}
 
