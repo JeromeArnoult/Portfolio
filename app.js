06645988c9207c3cabb7241711f7320a13344365
JeromeArnoult <jerome.arnoult.pro@proton.me>
2024-03-17 11:16:26 +0100

    Animation texte accueil typewriter

--- app.js
+++ app.js
@@ -16,4 +16,36 @@ if(window.matchMedia('(max-width: 1300px')) {
         })
     })
 }
+// Type Writer : 
 
+const txtAnim = document.querySelector('.txt-animation');
+
+  let typewriter = new Typewriter(txtAnim,  {
+    loop: false,
+    deleteSpeed: 20
+  })
+
+  typewriter 
+  .pauseFor(1800)
+  .changeDelay(20)
+  .typeString('Moi c\'est Jérôme Arnoult')
+  .pauseFor(300)
+  .typeString('<strong>, Développeur Full-Stack</strong> !')
+  .pauseFor(1000)
+  .deleteChars(13)
+  .typeString('<span style="color: #27ae60;"> CSS</span> !')
+  .pauseFor(1000)
+  .deleteChars(5)
+  .typeString('<span style="color: #EA39ff;"> PhP</span> !')
+  .pauseFor(1000)
+  .deleteChars(5)
+  .typeString('<span style="color: #EA39ff;"> Symfony</span> !')
+  .pauseFor(1000)
+  .deleteChars(9)
+  .typeString('<span style="color: midnightblue;"> Angular</span> !')
+  .pauseFor(1000)
+  .deleteChars(9)
+  .typeString('<span style="color: #ff6910;"> JavaScript</span> !')
+  .deleteChars(12)
+  .typeString('<span style="color: blue;"> Java </span> !')
+  .start()
